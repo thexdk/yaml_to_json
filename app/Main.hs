73dc11016d1579978c090d836f8e3c9e9db58282
Denis Kiselev <52455108+thexdk@users.noreply.github.com>
2023-04-28 23:49:46 +0300

    Add files via upload

--- app/Main.hs
+++ app/Main.hs
@@ -2,15 +2,28 @@ module Main (main) where
 
 import YamlToJson
 import System.IO
+import System.Environment
 
 main :: IO ()
 main = do
-    putStrLn "Enter file name:"
-    fileName <- getLine
-    fileHandle <- openFile "jsoned.txt" WriteMode
-    yaml_object <- readFile fileName
-    hPutStr fileHandle (to_json_str yaml_object)
-    putStrLn "The output is in file \"jsoned\""
-    hClose fileHandle
+    args <- getArgs
+    args_proc args
+
+
+args_proc :: [String] -> IO ()
+args_proc args = case args of
+    [] -> putStrLn "No input file given"
+    [x] -> files_proc x "jsoned.txt"
+    (x:y:_) -> files_proc x y
+
+files_proc :: String -> String -> IO ()
+files_proc fin fout = do
+                          yaml_object <- readFile fin
+                          fileHandle <- openFile fout WriteMode
+                          hPutStr fileHandle (to_json_str yaml_object)
+                          putStrLn ("The output is in file " ++ fout)
+                          hClose fileHandle
+
+
 
 
